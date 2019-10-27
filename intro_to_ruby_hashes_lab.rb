25ae5aa50c65e8ffca560959621eace7b65df237
Tanesha Goodman <t24good@icloud.com>
2019-10-27 14:56:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,10 +8,10 @@ def my_hash
     :message => "hello world"
   }
 my_hash
-  # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
+  
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
