50a2e0495715b93e5c4ed8d958f5016e48354ac7
Tanesha Goodman <t24good@icloud.com>
2019-10-27 14:54:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,6 +3,11 @@ def new_hash
 end
 
 def my_hash
+  my_hash = {
+    :created => time.now,
+    :message => "hello world"
+  }
+my_hash
   # return a valid hash with any key/value pair of your choice
 end
 
